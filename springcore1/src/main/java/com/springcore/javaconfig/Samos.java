4e650c6d6aa43606c8eafabeccbf60c67b73b939
Dineshmarasini <99908688+Dineshmarasini@users.noreply.github.com>
2023-08-21 23:36:22 +0545

    this is till 28th

---
+++ springcore1/src/main/java/com/springcore/javaconfig/Samos.java
@@ -0,0 +1,10 @@
+package com.springcore.javaconfig;
+
+public class Samos {
+	
+	
+	public void displayPrice() {
+		System.out.println("Price of samosa is : Rs 2 0");
+	}
+
+}
---
+++ springcore1/src/main/java/com/springcore/javaconfig/Student.java
@@ -0,0 +1,29 @@
+package com.springcore.javaconfig;
+
+import org.springframework.stereotype.Component;
+
+ 
+
+@Component("student")
+public class Student {
+	private Samos samosa; 
+	
+	public Student(Samos samosa) {
+		super();
+		this.samosa = samosa;
+	}
+
+	public Samos getSamosa() {
+		return samosa;
+	}
+
+	public void setSamosa(Samos samosa) {
+		this.samosa = samosa;
+	}
+
+	public void study() {
+		samosa.displayPrice();
+		System.out.println("student is reading book: ");
+	}
+
+}
